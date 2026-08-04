fcad3d8a25f28b9b15fc99c5b8043ab96be82167
Ritesh <43944441+mishrrit@users.noreply.github.com>
2026-08-04 19:47:08 +0530

    Refactor test cases in testDynamicId.spec.ts: improve naming, remove unnecessary page.close calls, and add new tests for client-side delay and event-based clicks

--- tests/uitestingPlayground/testDynamicId.spec.ts
+++ tests/uitestingPlayground/testDynamicId.spec.ts
@@ -1,7 +1,6 @@
 import { test, expect } from 'playwright/test';
 
-
-test.describe('test for elements', async () => {
+test.describe('test for elements', () => {
 
     test.beforeEach(async ({ page }) => {
         await page.goto('http://uitestingplayground.com/');
@@ -11,15 +10,14 @@ test.describe('test for elements', async () => {
         await page.close();
     });
 
-    test('Record buttomn click', async ({ page }) => {
+    test('Dynamic Id: Record buttomn click', async ({ page }) => {
 
         await page.getByRole('link', { name: 'Dynamic ID' }).click();
         await page.getByRole('button', { name: 'Button with Dynamic ID' }).click();
         await expect(page.getByRole('button', { name: 'Button with Dynamic ID' })).toBeVisible();
-        await page.close();
     });
 
-    test('Record primary (blue) button click and press ok in alert popup', async ({ page }) => {
+    test('Class Attribute: Record primary (blue) button click and press ok in alert popup', async ({ page }) => {
 
         await page.getByRole('link', { name: 'Class Attribute' }).click();
 
@@ -30,22 +28,17 @@ test.describe('test for elements', async () => {
         });
 
         await page.locator('button.btn-primary').click({ timeout: 15000 });
-        await page.waitForTimeout(1000);
-        await page.close();
     });
 
-    test('Execute the test to make sure that green button can not be hit twice.', async ({ page }) => {
+    test('Hidden Layers: Execute the test to make sure that green button can not be hit twice.', async ({ page }) => {
 
         await page.getByRole('link', { name: 'Hidden Layers' }).click();
         const greenButton = page.locator('#greenButton');
-
         await greenButton.click();
-
         await expect(greenButton).toBeEnabled();
 
         // Check if Playwright considers it actionable/clickable
         let clickable = true;
-
         try {
             await greenButton.click({
                 trial: true, // checks actionability without actually clicking
@@ -54,25 +47,51 @@ test.describe('test for elements', async () => {
         } catch {
             clickable = false;
         }
-
         expect(clickable).toBeFalsy();
-
-        await page.close();
     })
 
     test('Test for page loading and element visibility', async ({ page }) => {
 
         await page.getByRole('link', { name: 'Load Delay' }).click();
         await expect(page.getByRole('button', { name: 'Button Appearing After Delay' })).toBeVisible({ timeout: 10000 });
-        await page.close();
     })
 
-    test('Test for text visibility after clicking a button', async ({ page }) => {
+    test('AJAX Data:Test for text visibility after clicking a button', async ({ page }) => {
 
         await page.getByRole('link', { name: 'AJAX Data' }).click();
         await page.getByRole('button', { name: 'Button Triggering AJAX Request' }).click();
         await expect(page.getByText('Data loaded with AJAX get request.')).toBeVisible({ timeout: 16000 });
-        await page.close();
     })
+
+    test('Client Side Delay: Test for text visibility after clicking a button', async ({ page }) => {
+
+        await page.getByRole('link', { name: 'Client Side Delay' }).click();
+        await page.getByRole('button', { name: 'Button Triggering Client Side Logic' }).click();
+        await expect(page.getByText('Data calculated on the client side.')).toBeVisible({ timeout: 16000 });
+    })
+
+    test('Click: Event based click on DOM Event', async ({ page }) => {
+        await page.getByRole('link', { name: 'Click', exact: true }).click();
+        await page.getByRole('button', { name: 'Button That Ignores DOM Click Event' }).click();
+        await expect(page.locator('.btn-success')).toBeEnabled();
+    })
+
+    test('Text Input: Entering text into an element and verifying the input', async ({ page }) => {
+        await page.getByRole('link', { name: 'Text Input' }).click();
+        const inputField = page.locator('#newButtonName');
+        const inputText = 'Test Button Name';
+        await inputField.fill(inputText);
+        const buttonLabel = await page.locator('.btn-primary');
+        buttonLabel.click();
+        await expect(buttonLabel).toHaveText(inputText);
+    })
+
+    test('ScrollBar: Test for scrolling to an element and clicking it', async ({ page }) => {
+        await page.getByRole('link', { name: 'Scrollbars' }).click();
+        const button = page.locator('#hidingButton');
+        await button.click();
+        await expect(button).toBeVisible();
+    })
+
 });
 
