f9bb88fabd71f3b5dd07c04b08ecde591f825b7b
Ritesh <43944441+mishrrit@users.noreply.github.com>
2026-08-24 19:52:56 +0530

    Refactor testAddDeleteEmployee.spec.js to remove skip from Add Employee test and update imports

--- Fixtures/login.fixtures.ts
+++ Fixtures/login.fixtures.ts
@@ -1,26 +1,26 @@
-import { test as baseTest, Page } from '@playwright/test';
+import { test as baseTest, Page } from "@playwright/test";
 
-
-type LoginFixture = {
-    login: {
-        username: string;
-        password: string;
-    };
+export type LoginFixture = {
+  login: {
+    username: string;
+    password: string;
+  };
 };
 
 export const test = baseTest.extend<LoginFixture>({
-    login: async ({ page }: { page: Page }, use) => {
-        // Perform login steps here
-        await page.goto('https://example.com/login');
-        await page.fill('#username', 'your-username');
-        await page.fill('#password', 'your-password');
-        await page.click('#login-button');
-        // Wait for navigation after login
-        await page.waitForNavigation();
-        // Use the logged-in page in tests
-        //await use(page);
-    }
+  login: async ({ page }: { page: Page }, use) => {
+    const credentials = {
+      username: "Admin",
+      password: "admin123",
+    };
+
+    await page.goto(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
+    );
+    await page.fill("#username", credentials.username);
+    await page.fill("#password", credentials.password);
+    await page.click("#login-button");
+
+    await use(credentials);
+  },
 });
-
-export { LoginFixture };
-
--- tests/orangeHrm/testAddDeleteEmployee.spec.js
+++ tests/orangeHrm/testAddDeleteEmployee.spec.js
@@ -1,5 +1,6 @@
-//@ts-check 
-const { test, expect } = require("@playwright/test");
+//@ts-check
+const { expect } = require("@playwright/test");
+const { test } = require("../../Fixtures/login.fixtures.ts");
 require("../../pages/OrangeHrmLive/basePage");
 require("../../pages/OrangeHrmLive/commonHooks");
 const LoginPage = require("../../pages/OrangeHrmLive/loginPage");
@@ -7,13 +8,12 @@ const LoginPage = require("../../pages/OrangeHrmLive/loginPage");
 const PIMPage = require("../../pages/OrangeHrmLive/DashBoard/pim/empInfoPage");
 const AddEmployeePage = require("../../pages/OrangeHrmLive/DashBoard/pim/addEmployeePage");
 
-
-test.skip("Add Employee", async ({ page }) => {
+test("Add Employee", async ({ page }) => {
   const loginPage = new LoginPage(page);
   //const dashboardPage = new Dashboard(page);
   const pimPage = new PIMPage(page);
   const addEmployeePage = new AddEmployeePage(page);
-  await loginPage.login("Admin", "admin123");
+
   await loginPage.navigateTo("PIM");
   await pimPage.navigateToAddEmployee();
   await addEmployeePage.addEmployee("Test", "Playwright");
